217b75ccf22b0086bdde55dbd8c82b80332b0f78
Stefan Hoffmann <stho32github@gmail.com>
2021-04-17 19:18:38 +0200

    update :)

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update
+export DEBIAN_FRONTEND=noninteractive
+apt-get install xubuntu-core -y
+
+apt-get install x2goserver x2goserver-xsession -y
+
+apt install nodejs npm -y
+snap install --classic code -y
+
+useradd developer -d /home/developer -m ;
+echo -e "RH02JGUGIgQbwQViMW\nRH02JGUGIgQbwQViMW" | passwd developer
+usermod --shell /bin/bash developer
+echo "the account is setup"
+
